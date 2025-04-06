2fc3f3a62494c82f38a27a3c5cb257f1b4e8ff72
Joseph1205 <153535704+jos3ph1205@users.noreply.github.com>
2025-04-06 20:46:59 +0800

    Update and rename notes-unblur to notes-unblur.js

--- savemyexams/notes-unblur.js
+++ savemyexams/notes-unblur.js
@@ -1,3 +1,5 @@
+// PASTE THIS: javascript:window.location.href=https://raw.githubusercontent.com/jos3ph1205/minipass/refs/heads/main/savemyexams/notes-unblur
+
 javascript:(function(){
   document.head.insertAdjacentHTML("beforeend", `<style>
     [class*="revision-notes_blur"] {
